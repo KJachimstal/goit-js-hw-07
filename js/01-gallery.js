3ceb92f1b64ef156f3110bbc8addedae53703ebf
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-03-12 17:49:04 +0100

    Added dynamic generated images

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+
+const gallery = document.querySelector("div.gallery");
+let lightBox;
+
+for (const image of galleryItems) {
+  const newGalleryItem = document.createElement("div");
+  newGalleryItem.classList.add("gallery_item");
+
+  const newGalleryLink = document.createElement("a");
+  newGalleryLink.classList.add("gallery__link");
+  newGalleryLink.href = image.original;
+
+  const newImg = document.createElement("img");
+  newImg.classList.add("gallery__image");
+  newImg.src = image.preview;
+  newImg.alt = image.description;
+  newImg.setAttribute("data-source", image.original);
+
+  newGalleryLink.insertAdjacentElement("afterbegin", newImg);
+  newGalleryItem.insertAdjacentElement("afterbegin", newGalleryLink);
+  gallery.insertAdjacentElement("afterbegin", newGalleryItem);
+}
+
+// document.querySelector("img.gallery__image").onclick = () => {
+//   basicLightbox
+//     .create(
+//       `
+// 		<img width="1400" height="900" src="https://placehold.it/1400x900">
+// 	`
+//     )
+//     .show();
+// };
 
 console.log(galleryItems);
