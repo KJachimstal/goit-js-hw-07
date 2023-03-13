48cc294692cd8577a6a6eeae7045c54d695c8567
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-03-13 15:44:14 +0100

    Start secod task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+
+const gallery = document.querySelector("ul.gallery");
+
+for (const image of galleryItems) {
+  const galleryItem = document.createElement("a");
+  galleryItem.classList.add("gallery__item");
+
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = image.preview;
+  img.alt = image.description;
+  img.setAttribute("data-source", image.original);
+
+  galleryItem.insertAdjacentElement("afterbegin", img);
+  gallery.insertAdjacentElement("afterbegin", galleryItem);
+}
+
+console.log(gallery);
+
+gallery.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  gallery.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+    }
+  });
+}
 
 console.log(galleryItems);
